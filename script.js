f2d2f4e9c8a60840150957d64cc53cfd26dd8632
Tegar Muhamad Zidan <zidantegarmuhammad@gmail.com>
2025-08-07 22:54:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("nailongForm").addEventListener("submit", function(e) {
   let cerita = document.getElementById("cerita").value;
 
   // Ganti dengan nomor WhatsApp kamu (tanpa +, pakai 62)
-  let nomorWhatsApp = "6281234567890";
+  let nomorWhatsApp = "6283824581436";
 
   let pesan = `Hai Nailong 🐸%0AHari ini: ${seru}%0ARate: ${rate}/10%0ACerita: ${cerita}`;
   let url = `https://wa.me/${nomorWhatsApp}?text=${encodeURIComponent(pesan)}`;
